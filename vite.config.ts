2bebdf78527ef2fac2d2cebb36cf3b5e400bfbcc
Chris Lowcher <chrislowcher@gmail.com>
2025-05-22 17:47:02 -0400

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/rocket-league-tailwind/",
+  publicDir: "public",
   plugins: [
     vue(),
     vueDevTools(),
